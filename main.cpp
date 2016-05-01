241e6fa152450149a23689d93ca9637a680a84f2
root <root@debian.Gabriel>
2016-05-01 08:08:35 +0200

    second version

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
 #include "curl/include/curl/curl.h"
 
+
+static size_t writeUrlContent(void* content,size_t size,size_t mmemb,void* userp)
+{
+((std::string*)userp)->append((char*)content,size* mmemb);
+	return size*mmemb;
+}
+
 int main(){
-CURL *curl;
+  std::string readBuffer;
+  CURL *curl;
   CURLcode res;
  
   curl = curl_easy_init();
   if(curl) {
     curl_easy_setopt(curl, CURLOPT_URL, "http://nn7.free.fr");
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
+    //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,writeUrlContent);
+    curl_easy_setopt(curl,CURLOPT_WRITEDATA,&readBuffer);	
     res = curl_easy_perform(curl);
+   /*
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
- 
+    */
     curl_easy_cleanup(curl);
-   }
+    
+std::cout << "BEGINNE the buffer is "<<readBuffer<<" *********"<<std::endl;   
+}
 return 0;
 }
 

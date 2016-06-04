704a0a5c0973d317062c1fcec13230995701f722
Fabien <gamelinfabien@gmail.com>
2016-06-04 16:24:53 +0200

    tests

--- main.cpp
+++ main.cpp
@@ -8,7 +8,28 @@ static size_t writeUrlContent(void* content,size_t size,size_t mmemb,void* userp
 ((std::string*)userp)->append((char*)content,size* mmemb);
 	return size*mmemb;
 }
+ static ustring parseBody(size_t content)
+ {
+ // Parse the file
+    DomParser parser;
+    //parser.parse_file("file.xml");
+    parser.parse_memory("<html><head><a href='http://gabriel.com'> mon site </a><a href='http://free.fr'> mon site 2 </a></head></html>");
+    Node* rootNode = parser.get_document()->get_root_node();
 
+    // Xpath query
+    NodeSet result = rootNode->find("/html/head/a/@href");
+
+    // Get first node from result
+    for(int i = 0;i<result.size();i++){
+    Node *firstNodeInResult = result.at(i);
+    // Cast to Attribute node (dynamic_cast on reference can throw [fail fast])
+    Attribute &attribute = dynamic_cast<Attribute&>(*firstNodeInResult);
+    
+    // Get value of the attribute
+    ustring attributeValue = attribute.get_value();
+	}
+
+    return attributeValue;
 int main(){
   std::string readBuffer;
   CURL *curl;
